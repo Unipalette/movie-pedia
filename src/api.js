ad3356e9c74eaeae2303e83e2d39c2a6a2c73536
Unipalette <wjdtmddbs16@naver.com>
2022-08-31 19:36:51 +0900

    add

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export async function getReviews() {
+  const response = await fetch("https://learn.codeit.kr/4603/film-reviews");
+  // 배열 형태를 가진 json 데이터가 프로미스 객체에 성공결과로 담긴다.
+  const body = await response.json();
+  //자바스크립트에서도 사용가능한 배열과 객체 형태로 변환해주는 매소드
+  return body;
+  // 결국에는 데이터가 담긴 배열로 리턴이 된다.
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
 import ReveiwList from "./ReviewList";
-import items from "../mock.json";
 import { useState } from "react";
+import { getReviews } from "../api";
 
 function App() {
+  const [items, setItmes] = useState([]);
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
   const handleBestClick = () => setOrder("rating");
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => id !== item.id);
+    setItmes(nextItems);
+  };
+
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItmes(reviews);
+  };
 
   return (
     <>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleBestClick}>베스트순</button>
-      <ReveiwList items={sortedItems} />
+      <ReveiwList items={sortedItems} onDelete={handleDelete} />
+
+      <button onClick={handleLoadClick}>불러오기</button>
     </>
   );
 }
